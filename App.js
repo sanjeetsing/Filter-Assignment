da563b8e68a02bc7213323306d2bb6c30edb86c7
Sanjeet Singh <sanjeetsinghsolanki11@gmail.com>
2024-08-06 21:14:10 +0530

    Assignment

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading" },
@@ -17,7 +20,7 @@
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "i am an h1 tag"),
+    React.createElement("h1", {}, "Hello everyone"),
     React.createElement("h2", {}, "i am an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
--- READM.md
+++ READM.md
@@ -1,1 +1,21 @@
 # Namstea React
+
+
+# parcel
+- dev build 
+- local server
+- HMR(hot module replacement)
+- File watching Alogoritham - written in c++
+- caching - Faster Build
+- image optimization
+- Minification
+- Bundling
+- compress
+- consistant Hashing
+- code Splitting
+- Differential Bundling - support older browser
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree shaking - remove unused codes
+- Different dev and prod bundles
